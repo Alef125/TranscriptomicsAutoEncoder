d5a96e35e8b19dc17bd861db3cc52c5d159bc914
Alef125 <alifathi.125@gmail.com>
2022-04-22 19:59:18 +0430

    BioNNDatasets new class added for cancer data

--- BioNNDatasets.py
+++ BioNNDatasets.py
@@ -5,6 +5,7 @@ This script, provides classes to builds a torch.utils.data.Dataset object based 
 from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
+import os
 
 
 def make_sorted_list_of_expressions(expressions_dict: dict) -> np.ndarray:
@@ -12,14 +13,18 @@ def make_sorted_list_of_expressions(expressions_dict: dict) -> np.ndarray:
     :param expressions_dict: A dictionary of genes expressions in the format of {gene_id: gene_expression}
     :return: Array of genes expressions, sorted by genes ids
     """
-    _num_genes = max(list(expressions_dict.keys())) + 1
+    _num_genes = max(list(expressions_dict.keys())) + 1  # It is an acceptable assumption :))
     _sorted_expressions = np.zeros(_num_genes)
     for _gene_id, _gene_expr in expressions_dict.items():
         _sorted_expressions[_gene_id] = _gene_expr
     return _sorted_expressions
 
 
-class CustomTranscriptomicsDataset(Dataset):
+class TissuesTranscriptomicsDataset(Dataset):
+    """
+    A Dataset object for Tissue Transcriptomics Data,
+        suitable when all transcriptional data are saved in a single dataframe
+    """
     def __init__(self,
                  annotations_file: str,
                  dataset_dir: str,
@@ -57,6 +62,44 @@ class CustomTranscriptomicsDataset(Dataset):
     def __getitem__(self, idx):
         expressions_list = self._get_expressions(tissue_label=self.dataset_labels.iloc[idx, 1])
         label = self.dataset_labels.iloc[idx, 1]  # Dummy for our purpose
+        if self.transform:
+            expressions_list = self.transform(expressions_list)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return expressions_list, label
+
+
+class CustomTranscriptomicsDataset(Dataset):
+    """
+    A Dataset object for Tumors Transcriptomics Data,
+        suitable when transcriptional data are saved in separated files
+    """
+    def __init__(self,
+                 annotations_file: str,
+                 dataset_dir: str,
+                 transform=None, target_transform=None):
+        """
+        :param :param annotations_file: The filepath for the annotation.csv file,
+                                 including "SampleFile.csv, CancerType" in each column
+        :param dataset_dir: The direction of the folder in which samples' expressions .csv files are saved
+        :param transform: -
+        :param target_transform: -
+        """
+        self.dataset_labels = pd.read_csv(annotations_file, names=['FileName', 'Label'])
+        self.dataset_dir = dataset_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.dataset_labels)
+
+    def __getitem__(self, idx):
+        sample_filepath = os.path.join(self.dataset_dir, self.dataset_labels.iloc[idx, 0])
+        sample_df = pd.read_csv(sample_filepath)
+        gene_ids = [int(gene_identifier[1:]) for gene_identifier in sample_df['GeneID']]  # Extracts *** from 'G***'
+        expressions = sample_df[sample_df.columns[-1]]
+        expressions_list = make_sorted_list_of_expressions(expressions_dict=dict(zip(gene_ids, expressions)))
+        label = self.dataset_labels.iloc[idx, 1]  # Cancer Type
         if self.transform:
             expressions_list = self.transform(expressions_list)
         if self.target_transform:
