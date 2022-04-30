7c51044bfc0c25052d5fb88122f889b70e59a474
Alef125 <alifathi.125@gmail.com>
2022-04-30 15:14:39 +0430

    BioAE_Loss output separation and mse modified

--- BioAE_Loss.py
+++ BioAE_Loss.py
@@ -7,7 +7,8 @@ from torch import Tensor
 import torch.nn.functional as F
 
 
-LAMBDA = 1000
+LAMBDA1 = 100
+LAMBDA2 = 5
 
 
 class AESSLoss(Module):
@@ -37,17 +38,28 @@ class AESSLoss(Module):
         :return: Auto Encoder loss + lambda * Steady State (Sv) Loss
         """
         # ########## Extracting Parameters ##########
-        batch_size = network_output.shape[0]  # Should be equal to ae_target.shape[0]
-        num_output_neurons = network_output.shape[1]
-        num_genes = ae_target.shape[1]
-        num_metabolites = num_output_neurons - num_genes
+        # batch_size = network_output.shape[0]  # Should be equal to ae_target.shape[0]
+        # num_output_neurons = network_output.shape[1]
+        # num_genes = ae_target.shape[1]
+        # num_metabolites = num_output_neurons - num_genes
         # ########## Splitting the Output ##########
-        ae_output = network_output[:, :num_genes]
-        ss_output = network_output[:, num_genes:]
-        # ######### Calculating AE and SS Losses ###########
+        # ae_output = network_output[:, :num_genes]
+        # ss_output = network_output[:, num_genes:]
+        ae_output = network_output['Decoded']
+        ss_output = network_output['Metabolites']
+        rxn_output = network_output['Full_Reactions']
+        # ######### Calculating AE, SS, and Parsimony Losses ###########
         ae_loss = F.l1_loss(ae_output, ae_target, reduction=self.reduction)
-        sv_zero_vec = torch.zeros((batch_size, num_metabolites))
-        ss_loss = F.l1_loss(ss_output, sv_zero_vec, reduction=self.reduction)  # or torch.norm(ss_output, 1)
+        # ###########
+        # sv_zero_vec = torch.zeros((batch_size, num_metabolites)))
+        ss_loss = F.mse_loss(ss_output, torch.zeros(ss_output.shape), reduction=self.reduction)
+        # ss_loss = torch.norm(ss_output, 1) / (ss_output.shape[0] * ss_output.shape[1])
+        # ############
+        # reactions_zero_vec = torch.zeros((batch_size, num_reactions))
+        parsimony_loss = F.l1_loss(rxn_output, torch.zeros(rxn_output.shape), reduction=self.reduction)
+        # parsimony_loss = torch.norm(rxn_output, 1)/ (rxn_output.shape[0] * rxn_output.shape[1])
+        # ############
         # loss_weights = torch.cat((torch.ones(num_genes), LAMBDA * torch.ones(NUM_METs)), dim=0)
-        # result = (loss_weights * torch.abs(output - target)).mean()
-        return ae_loss + LAMBDA * ss_loss
+        # total_loss = (loss_weights * torch.abs(output - target)).mean()
+        total_loss = ae_loss + LAMBDA1 * ss_loss + LAMBDA2 * parsimony_loss
+        return total_loss
